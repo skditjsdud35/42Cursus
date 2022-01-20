fb1074ac41eeea3977517e9af9a73883d78f558c
Ahn sun young <68502219+skditjsdud35@users.noreply.github.com>
2022-01-20 12:17:44 +0900

    Create repeat_alpha.c

---
+++ Exam/repeat_alpha.c
@@ -0,0 +1,28 @@
+#include <unistd.h>
+
+int main(int ac, char **av){
+
+        int i = 0;
+        int count = 1;
+
+        if(ac == 2)
+        {
+                while(av[1][i] != '\0')
+                {
+                        if(av[1][i] >= 'A' && av[1][i] <= 'Z')
+                                count = av[1][i] - 64;
+                        else if(av[1][i] >= 'a' && av[1][i] <= 'z')
+                                count = av[1][i] - 96;
+
+                        while(count > 0)
+                        {
+                                write(1, &av[1][i], 1);
+                                count--;
+                        }
+                        count = 1;
+                        i++;
+                }
+        }
+        write(1, "\n", 1);
+        return 0;
+}
