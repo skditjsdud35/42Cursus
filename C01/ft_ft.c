7270116514ca79d70134a9f50f8d60c93b943a22
Ahn sun young <68502219+skditjsdud35@users.noreply.github.com>
2022-01-24 23:48:47 +0900

    Create ft_ft.c

---
+++ C01/ft_ft.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+//#include <stdio.h>
+
+void ft_ft(int *nbr)
+{
+        *nbr = 42;
+}
+
+//int main()
+//{
+//        int n;
+//
+//        ft_ft(&n);
+//
+//        printf("%d \n", n);
+//}
